7f1dd8c2247010623a2817d0c3ffe582ff003a4f
onezeros <lizhijie@wushuikeji.com>
2025-03-01 18:54:00 +0800

    add yolo fine-tune configurefile:self-seg.yaml

---
+++ self_train.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+# Load a model
+# model = YOLO('yolov8n-seg.yaml')  # build a new model from YAML
+model = YOLO('yolov5s.pt')  # load a pretrained model (recommended for training)
+# model = YOLO('yolov8n-seg.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
+
+# Train the model
+results = model.train(data='self-seg.yaml', epochs=5, imgsz=640, workers=0,patience=10,batch=32)
+# esults = model.train(data='self-seg.yaml', epochs=100)
+
