dfcbde6977d13358b57aa16071534c6a93ff4912
onezeros <lizhijie@wushuikeji.com>
2025-03-03 16:38:56 +0800

    add labelimg

--- self_train.py
+++ self_train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO('yolov8n-seg.yaml')  # build a new model from YAML
-model = YOLO('yolov5s.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov11n-seg.pt')  # load a pretrained model (recommended for training)
 # model = YOLO('yolov8n-seg.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
